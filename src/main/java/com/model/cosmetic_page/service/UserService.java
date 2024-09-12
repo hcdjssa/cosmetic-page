a471c4285c3a095b654e1d8a127a92ff00cf71b6
hcdjssa <128838445+hcdjssa@users.noreply.github.com>
2024-09-12 23:51:03 +0530

    service

--- src/main/java/com/model/cosmetic_page/service/UserService.java
+++ src/main/java/com/model/cosmetic_page/service/UserService.java
@@ -1,5 +1,19 @@
 package com.model.cosmetic_page.service;
 
-public class UserService {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import com.model.cosmetic_page.entity.User;
+import com.model.cosmetic_page.repository.UserRepository;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+    
+    public User loginUser(String username, String password) {
+        return userRepository.findByUsername(username)
+            .filter(user -> user.getPassword().equals(password))  // Use BCrypt in production
+            .orElse(null);
+    }
 }
